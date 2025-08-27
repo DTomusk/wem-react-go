2520febea14bd79b5029e6e557fec0e9387f7812
Dave <david.tomusk@gmail.com>
2025-08-27 21:32:50 +0100

    Ensure checked behaviour is consistent

--- web/src/components/molecules/ThemeToggle.tsx
+++ web/src/components/molecules/ThemeToggle.tsx
@@ -3,11 +3,13 @@ import { useThemeMode } from "../organisms/ThemeRegistry";
 
 export default function ThemeToggle() {
     const { mode, toggleTheme } = useThemeMode();
+
     return (
         <FormControlLabel control={
             <Switch onClick={toggleTheme}/>} 
             label={mode === "light" ? "Light" : "Dark"}
             labelPlacement="start"
+            checked={mode === "dark"}
             />
     )
 }
